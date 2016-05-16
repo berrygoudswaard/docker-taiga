03ccc8cb513ff98803661f3ec1500a94e4124a54
Berry Goudswaard <berry@atom.nl>
2016-05-16 21:27:08 +0200

    Fix static and media file access through nginx

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 function config {
     DEBUG=${DEBUG:-False}
-    ADMIN_NAME=${ADMIN_NAME:-Admin}
-    ADMIN_EMAIL=${ADMIN_EMAIL:-example@example.com}
+    MEDIA_ROOT=${MEDIA_ROOT:-/home/taiga/media}
+    STATIC_ROOT=${STATIC_ROOT:-/home/taiga/static}
     TEMPLATE_DEBUG=${TEMPLATE_DEBUG:-False}
     PUBLIC_REGISTER_ENABLED=${PUBLIC_REGISTER_ENABLED:-False}
-    HOST_NAME=${HOST_NAME:-localhost}
+    HOSTNAME=${HOSTNAME:-localhost}
     SECRET_KEY=${SECRET_KEY:-theveryultratopsecretkey}
     DEFAULT_FROM_EMAIL=${DEFAULT_FROM_EMAIL:-no-reply@example.com}
     SERVER_EMAIL=${SERVER_EMAIL:-no-reply@example.com}
@@ -18,23 +18,19 @@ function config {
 
     echo "from .development import *
 
-ADMINS = (
-    ('${ADMIN_NAME}', '${ADMIN_EMAIL}'),
-)
-
 DEBUG = ${DEBUG}
 TEMPLATE_DEBUG = ${TEMPLATE_DEBUG}
 PUBLIC_REGISTER_ENABLED = ${PUBLIC_REGISTER_ENABLED}
 SECRET_KEY = '${SECRET_KEY}'
 
-MEDIA_ROOT = '/home/taiga/taiga-back/taiga/base/tic/media'
-STATIC_ROOT = '/home/taiga/taiga-back/taiga/base/static'
+MEDIA_ROOT = '${MEDIA_ROOT}'
+STATIC_ROOT = '${STATIC_ROOT}'
 
-MEDIA_URL = 'http://${HOST_NAME}/media/'
-STATIC_URL = 'http://${HOST_NAME}/static/'
-ADMIN_MEDIA_PREFIX = 'http://${HOST_NAME}/static/admin/'
+MEDIA_URL = 'http://${HOSTNAME}/media/'
+STATIC_URL = 'http://${HOSTNAME}/static/'
+ADMIN_MEDIA_PREFIX = 'http://${HOSTNAME}/static/admin/'
 SITES['front']['scheme'] = 'http'
-SITES['front']['domain'] = '${HOST_NAME}'
+SITES['front']['domain'] = '${HOSTNAME}'
 
 DEFAULT_FROM_EMAIL = '${DEFAULT_FROM_EMAIL}'
 SERVER_EMAIL = '{SERVER_EMAIL}'
@@ -52,27 +48,12 @@ DATABASES = {
 " > settings/local.py
 }
 
-if [ "$1" = 'init' ]; then
-    config
-    python manage.py migrate --noinput
-    python manage.py loaddata initial_user
-    python manage.py loaddata initial_project_templates
-    python manage.py loaddata initial_role
-    exit
-fi
-
-if [ "$1" = 'migrate' ]; then
-    config
-    python manage.py migrate --noinput
-    exit
-fi
-
 if [ "$1" = 'taiga' ]; then
     config
+    sudo python manage.py collectstatic --noinput
+    sudo python manage.py migrate --noinput
     python manage.py runserver 0.0.0.0:8000
     exit
 fi
 
 exec "$@"
-
-
